b8a74e01fac0324982025525e7d4d2c334d3ff9a
nguyenquangvinh2000 <vinhng74@gmail.com>
2020-11-10 00:38:58 +0700

    a

--- binary tree/list.h
+++ binary tree/list.h
@@ -7,17 +7,43 @@
 
 #ifndef list_h
 #define list_h
+#include"node.h"
 template<class T>
 class list{
 private:
     node<T> *head;
-    node<T> *tail;
 public:
     list(){
         head = 0;
-        tail = 0;
     }
-    void add
+    void additem(T item)
+    {
+        node<T> *p = new node<T>;
+        p->data = item;
+        p->next = 0;
+        
+        if(head == 0)
+        {
+            head = p;
+        }
+        else{
+            node<T> *p = head;
+            while (p->next!=0) {
+                p=p->next;
+            }
+            p->next = item;
+        }
+    }
+    T getItem(int index){
+        node<T> *p = new node<T>;
+        for(int i=1;i<index;i++)
+        {
+            p=p->next;
+        }
+        return p->data;
+    }
+    
+    
 };
 
 #endif /* list_h */
